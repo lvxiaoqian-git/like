1cacfc9d89e88392119e85727e419bee397f920b
吕笑乾 <lvxiaoqian.zwld@bytedance.com>
2021-09-09 14:20:46 +0800

    修改：     src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { ReactElement } from "react";
+import { ReactElement, useEffect } from "react";
+import { Index } from "./router";
 
 function App(): ReactElement<any, any> {
+  useEffect(() => {
+    console.log(global);
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Index />
     </div>
   );
 }
