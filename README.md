1c0870476a7e7ba58c82dc6492d7950d61ba1981
吕笑乾 <lvxiaoqian.zwld@bytedance.com>
2021-09-13 17:05:27 +0800

    feat: 完善首页路由跳转和过渡效果之间的联系

--- README.md
+++ README.md
@@ -6,20 +6,20 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 
 In the project directory, you can run:
 
-### `yarn start`
+### `yarn start` or `npm start`
 
 Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+Open [http://localhost:8088](http://localhost:8088) to view it in the browser.
 
 The page will reload if you make edits.\
 You will also see any lint errors in the console.
 
-### `yarn test`
+### `yarn test` or `npm test`
 
 Launches the test runner in the interactive watch mode.\
 See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
 
-### `yarn build`
+### `yarn build` or `npm build`
 
 Builds the app for production to the `build` folder.\
 It correctly bundles React in production mode and optimizes the build for the best performance.
@@ -29,9 +29,11 @@ Your app is ready to be deployed!
 
 See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
 
-### `yarn eject`
+### `yarn eject` or  `npm eject`
 
 **Note: this is a one-way operation. Once you `eject`, you can’t go back!**
+
+- eject 用于喷射出 webpack 配置，当然他是单项的，现已喷射
 
 If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
 
--- src/pages/p/Home/index.tsx
+++ src/pages/p/Home/index.tsx
@@ -1,41 +1,61 @@
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Route, useHistory } from "react-router-dom";
-
 import "./index.less";
 export interface HomeProps {
   pathname?: string;
+  location?: { pathname: string };
 }
 
 function Life() {
-  return <div>
-    dsadsada
-    <p>dsa</p>
-    <p>dsa</p>
-    <p>dsa</p>
-    <p>dsa</p>
-    <p>dsa</p>
-    <p>dsa</p>
-    <p>dsa</p>
-    <p>dsa</p>
-    <p>dsa</p>
-    <p>dsa</p>
-  </div>
+  return (
+    <div>
+      dsadsada
+      <p>dsa</p>
+      <p>dsa</p>
+      <p>dsa</p>
+      <p>dsa</p>
+      <p>dsa</p>
+      <p>dsa</p>
+      <p>dsa</p>
+      <p>dsa</p>
+      <p>dsa</p>
+      <p>dsa</p>
+    </div>
+  );
 }
 
 export function Home(props: HomeProps) {
   const history = useHistory();
+  const { location } = props;
   const [move, setMove] = useState(false);
+
+  useEffect(() => {
+    console.log(location?.pathname);
+    if (location?.pathname === "/home") {
+      // 本来要加路由监听，突然发现hooks中的effect hook可以结合props来监听路由
+      // 那么home就可以直接作为页面的layout来使用啦
+      setMove(false);
+    }
+  }, [location?.pathname]);
 
   return (
     <div className="home">
-      <div className={`home__study ${move ? "home__study-move" : ""}`} onClick={() => {
-        history.push('/home/life')
-        setMove(true)
-      }}></div>
-      <div className={`home__live ${move ? "home__live-move" : ""}`} onClick={() => {
-        history.push('/home/life')
-        setMove(true)
-      }}></div>
+      {/* 自己写过渡，好奇怪，后期用库吧，动画是此项目的核心之一 */}
+      <div
+        className={`home__study ${move ? "home__study-move" : ""}`}
+        onClick={() => {
+          history.push("/home/life");
+          setMove(true);
+        }}
+      ></div>
+      <div
+        className={`home__live ${move ? "home__live-move" : ""}`}
+        onClick={() => {
+          history.push("/home/life");
+          setMove(true);
+        }}
+      ></div>
+      {/* 直接跳转子路由，这里的我觉得还不错，但是具有优化的地方，路由可以集中管理，毕竟是"单页面应用" */}
       <Route path="/home/life" component={Life}></Route>
     </div>
   );
